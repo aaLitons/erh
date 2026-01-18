6101fa64a767f5f3b4cd392fb68cee57957bf13e
aaLitons <aalitons.mg@gmail.com>
2026-01-18 10:27:51 +0300

    feat: update the sidebar component for more compatibility

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -86,7 +86,7 @@ const Sidebar = () => {
       >
         <div className="flex items-baseline justify-between p-4">
           <h3 className="text-xl font-semibold">Menu</h3>
-          <Button variant="outline" size="icon" onClick={close} aria-label="Close sidebar">
+          <Button variant="outline" className="bg-transparent" size="icon" onClick={close} aria-label="Close sidebar">
             <X />
           </Button>
         </div>
@@ -94,38 +94,10 @@ const Sidebar = () => {
         <NavigationMenu orientation="vertical">
           <NavigationMenuList className="flex-col">
         <NavigationMenuItem>
-          <NavigationMenuTrigger>Home</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <ul className="grid gap-2 md:w-100 lg:w-125 lg:grid-cols-[.75fr_1fr]">
-              <li className="row-span-3">
-                <NavigationMenuLink asChild>
-                  <a
-                    className="from-muted/50 to-muted flex h-full w-full flex-col justify-end rounded-md bg-linear-to-b p-4 no-underline outline-hidden transition-all duration-200 select-none focus:shadow-md md:p-6"
-                    href="/"
-                  >
-                    <div className="mb-2 text-lg font-medium sm:mt-4">
-                      shadcn/ui
-                    </div>
-                    <p className="text-muted-foreground text-sm leading-tight">
-                      Beautifully designed components built with Tailwind CSS.
-                    </p>
-                  </a>
-                </NavigationMenuLink>
-              </li>
-              <ListItem href="/docs" title="Introduction">
-                Re-usable components built using Radix UI and Tailwind CSS.
-              </ListItem>
-              <ListItem href="/docs/installation" title="Installation">
-                How to install dependencies and structure your app.
-              </ListItem>
-              <ListItem href="/docs/primitives/typography" title="Typography">
-                Styles for headings, paragraphs, lists...etc
-              </ListItem>
-            </ul>
-          </NavigationMenuContent>
+          <Link href="#" className="flex items-center px-2 py-2 rounded hover:bg-accent"><Home size={18}/> <p className="px-2 text-sm">Acceuil</p></Link>
         </NavigationMenuItem>
         <NavigationMenuItem>
-          <NavigationMenuTrigger>Components</NavigationMenuTrigger>
+          <NavigationMenuTrigger><Link href="#" className="flex items-center rounded" ><Book size={18}/> <p className="px-2 text-sm">Cours</p></Link></NavigationMenuTrigger>
           <NavigationMenuContent>
             <ul className="grid gap-2 sm:w-100 md:w-125 md:grid-cols-2 lg:w-150">
               {components.map((component) => (
@@ -140,46 +112,8 @@ const Sidebar = () => {
             </ul>
           </NavigationMenuContent>
         </NavigationMenuItem>
-        <NavigationMenuItem>
-          <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
-            <Link href="/docs">Docs</Link>
-          </NavigationMenuLink>
-        </NavigationMenuItem>
         <NavigationMenuItem className="hidden md:block">
-          <NavigationMenuTrigger>List</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <ul className="grid w-75 gap-4">
-              <li>
-                <NavigationMenuLink asChild>
-                  <Link href="#">
-                    <div className="font-medium">Components</div>
-                    <div className="text-muted-foreground">
-                      Browse all components in the library.
-                    </div>
-                  </Link>
-                </NavigationMenuLink>
-                <NavigationMenuLink asChild>
-                  <Link href="#">
-                    <div className="font-medium">Documentation</div>
-                    <div className="text-muted-foreground">
-                      Learn how to use the library.
-                    </div>
-                  </Link>
-                </NavigationMenuLink>
-                <NavigationMenuLink asChild>
-                  <Link href="#">
-                    <div className="font-medium">Blog</div>
-                    <div className="text-muted-foreground">
-                      Read our latest blog posts.
-                    </div>
-                  </Link>
-                </NavigationMenuLink>
-              </li>
-            </ul>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-        <NavigationMenuItem className="hidden md:block">
-          <NavigationMenuTrigger>Simple</NavigationMenuTrigger>
+          <NavigationMenuTrigger><Link href="#" className="flex items-center rounded hover:bg-accent" ><Pen size={18}/> <p className="px-2 text-sm">Exercices</p></Link></NavigationMenuTrigger>
           <NavigationMenuContent>
             <ul className="grid w-50 gap-4">
               <li>
@@ -197,7 +131,7 @@ const Sidebar = () => {
           </NavigationMenuContent>
         </NavigationMenuItem>
         <NavigationMenuItem className="hidden md:block" >
-          <NavigationMenuTrigger>With Icon</NavigationMenuTrigger>
+          <NavigationMenuTrigger><Link href="#" className="flex items-center rounded hover:bg-accent" ><Contact size={18}/> <p className="px-2 text-sm">Besoin d'aide</p></Link></NavigationMenuTrigger>
           <NavigationMenuContent >
             <ul className="grid w-50 gap-4">
               <li>
